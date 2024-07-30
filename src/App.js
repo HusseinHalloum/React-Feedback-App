5ba68676fff0b3163ef2a5c0740b167e55a2211a
HusseinHalloum <hseinhalloum00@gmail.com>
2024-07-30 14:15:56 +0300

    Remove unused variables from App.js and FeedbackList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
 import Header from './components/Header'
-import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
@@ -10,8 +8,6 @@ import AboutPage from './pages/AboutPage'
 import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
   return (
     <FeedbackProvider>
       <Router>
